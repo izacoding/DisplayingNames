caad57ff54909afcbb0559f7cd52984f240e44a2
izacoding <dsteele008@gmail.com>
2018-05-31 12:55:03 -0400

    New jAVAScript

---
+++ script.js
@@ -0,0 +1,12 @@
+  
+function getFormValue(){
+   var fname= document.getElementById('fname').value;
+
+   console.log(fname);
+
+   var lname= document.getElementById('lname').value;
+   console.log(lname);
+
+   document.getElementById("display").innerHTML  = "Hello my name is" + fname + " " + lname
+};
+
